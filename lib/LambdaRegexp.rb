c1833491b13e8aebc889c81bcc0d261cb04f0b2b
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-12-02 19:51:03 +0000

    Añadido rdoc y toda la documentación necesaria en el código. Cambiada la licencia.

--- lib/LambdaRegexp.rb
+++ lib/LambdaRegexp.rb
@@ -7,26 +7,50 @@ module ULL
 
                 module_function
 
+                # Transición épsilon:
+                #
+                # * Devuelve la entrada sin consumir ningun símbolo.
                 def epsilon
                     ->(x) { x }
                 end
 
+                # Constante literal:
+                #
+                # * Crea una expresion regular que es una constante literal.
+                # * Devuelve <em>false</em> si <b>c</b> no es prefijo de <b>x</b>.
+                # * Devuelve el resto que no case con <b>x</b>, si hubo matching.
                 def char(c) # c: Cadena comprobar si casa 'x' que es la RegExp
                     ->(x) { x[0, c.length] == c and x[c.length..-1] } # x[c.length..-1] Desde donde no casó hasta el final
                 end
 
+                # Concatenación:
+                #
+                # * Concatena dos expresiones regulares.
+                # * Devuelve <em>false</em> si no se produce matching.
+                # * Devuelve el resto que no case con <b>l</b> concatenado con <b>r</b>, si hubo matching.
                 def seq(l, r) # Recibe RegExp (Lambdas) a la izquierda y a la derecha
                     ->(x) { w = l[x] and r[w] }
                 end
 
+                # Disyunción:
+                #
+                # * Concatena dos expresiones regulares.
+                # * Devuelve <em>false</em> si no se produce matching.
+                # * Devuelve el resto que no case con <b>l</b> o con <b>r</b>, si hubo matching.
                 def alt(l, r) # Recibe RegExp (Lambdas) a la izquierda y a la derecha
                     ->(x) { l[x] or r[x] }
                 end
 
+                # Cierre positivo:
+                #
+                # * Realiza el cierre positivo de una expresión regular <b>r</b>
                 def plus(r)
                     ->(x) { seq(r, star(r))[x] }
                 end
 
+                # Cierre Kleene:
+                #
+                # * Realiza el cierre de Kleene de una expresión regular <b>r</b>
                 def star(r)
                     ->(x) { plus(r)[x] or epsilon[x] }
                 end
@@ -36,29 +60,50 @@ module ULL
 end
 
 class String
-    include ULL::ETSII::Alu3177::LambdaRegexp
+    include ULL::ETSII::Alu3177
 
+    # Constante literal:
+    #
+    # * Crea una expresion regular que es una constante literal.
+    # * Devuelve <em>false</em> si <b>c</b> no es prefijo de <b>x</b>.
+    # * Devuelve el resto que no case con <b>x</b>, si hubo matching.
     def re
-        char(self)
+        LambdaRegexp::char(self)
     end
 end
 
 class Proc
-    include ULL::ETSII::Alu3177::LambdaRegexp
+    include ULL::ETSII::Alu3177
 
+    # Concatenación:
+    #
+    # * Concatena dos expresiones regulares.
+    # * Devuelve <em>false</em> si no se produce matching.
+    # * Devuelve el resto que no case con <b>l</b> concatenado con <b>r</b>, si hubo matching.
     def - (r)
-        seq(self, r)
+        LambdaRegexp::seq(self, r)
     end
 
+    # Disyunción:
+    #
+    # * Concatena dos expresiones regulares.
+    # * Devuelve <em>false</em> si no se produce matching.
+    # * Devuelve el resto que no case con <b>l</b> o con <b>r</b>, si hubo matching.
     def | (r)
-        alt(self, r)
+        LambdaRegexp::alt(self, r)
     end
 
+    # Cierre positivo:
+    #
+    # * Realiza el cierre positivo de una expresión regular <b>r</b>
     def +@
-        plus(self)
+        LambdaRegexp::plus(self)
     end
 
+    # Cierre Kleene:
+    #
+    # * Realiza el cierre de Kleene de una expresión regular <b>r</b>
     def ~@
-        star(self)
+        LambdaRegexp::star(self)
     end
 end

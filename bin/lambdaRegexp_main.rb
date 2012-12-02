e0aa76a2e50c63b49b226786d5e6930ed376effd
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-12-02 18:44:27 +0000

    Eliminados apuntos del fichero lib/LambdaRegexp.rb. Se ha incluído en el bin/lambdaRegexp_main.rb el mismo programa que aparece al final de la descripción de la práctica; obteniendo los mismos resultados.

--- bin/lambdaRegexp_main.rb
+++ bin/lambdaRegexp_main.rb
@@ -2,17 +2,42 @@ require 'LambdaRegexp'
 
 include ULL::ETSII::Alu3177::LambdaRegexp
 
-a = char 'a'
-b = char 'b'
+e = 'cd'.re
+s = 'cde'
+remaining = e[s] # 'cde' Matched. Remaining = 'e'
+puts "/cd/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
 
-re = seq(a,b)
-puts re['ab']
+e = 'd'.re | 'r'.re
+s = 'rdddf'
+remaining = e[s] #  'rdddf' Matched. Remaining = 'dddf'
+puts "/d|r/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
 
-re = alt(a,b)
-puts re['ab']
+e = 'c'.re - ('d'.re | 'r'.re)
+s = 'crddf'
+remaining = e[s] # 'crddf' Matched. Remaining = 'ddf'
+puts "/c(d|r)/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
 
-re = plus(a)
-puts re['aaaa']
+e = 'c'.re - +('d'.re | 'r'.re)
+s = 'crddf'
+remaining = e[s]
+puts "/c(d|r)+/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
 
-re = star(a)
-puts re['aaaa']
+e = 'c'.re - ~('d'.re | 'r'.re)
+s = 'cdrdrf'
+remaining = e[s]
+puts "/c(d|r)*/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
+
+e = 'c'.re - ~('d'.re | 'r'.re)
+s = 'cfff'
+remaining = e[s]
+puts "/c(d|r)*/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
+
+e = 'c'.re - ~('d'.re | 'r'.re)
+s = 'cdrd'
+remaining = e[s]
+puts "/c(d|r)*/ '#{s}' Matched. Remaining = '#{remaining}'" if remaining
+
+e = 'c'.re - +('d'.re | 'r'.re)
+s = 'cfff'
+remaining = e[s] # 'cfff' didn't match. Remaining = 'false'
+puts "/c(d|r)+/ '#{s}' didn't match. Remaining = '#{remaining}'" unless remaining
--- lib/LambdaRegexp.rb
+++ lib/LambdaRegexp.rb
@@ -35,7 +35,6 @@ module ULL
     end
 end
 
-
 class String
     include ULL::ETSII::Alu3177::LambdaRegexp
 
@@ -63,61 +62,3 @@ class Proc
         star(self)
     end
 end
-
-
-=begin
-Apuntes en LAB:
-    array1=[1,2,3,4]
-    array2=[5,6,7,8]
-
-[**] Explicacion Bloques: (Un bloque es una estructura sintáctica)
-    array1.collect! do |n| # collect! = metodo enumerable q coge cada uno y hace con él lo que pone en el bloque
-        n**2 # Eleva al cuadrado
-    end
-    array2.collect! do |n|  # Tenemos que hacer lo mismo para el otro bloque...
-        n**2
-    end
-
-* Escribamos un metodo collect (pero con otro nombre, iterate!), que al ser de Array, debo hacerlo en la clase Array...
-
-class Array
-    def iterate!
-        self.each_index do |i, n|
-            self[i] = yield (n)
-        end
-end
-
-* Ahora llamamos a nuestro método
-
-    array1.iterate! do |n|
-        n**2 # Eleva al cuadrado
-    end
-    array2.iterate! do |n|
-        n**2
-    end
-
-[**] Ahora 'a la' PROC : Un proc es un objeto
-
-class Array
-    def iterate! (&code) # & implica proc
-        self.each_index do |i, n|
-            self[i] = code.call (n)
-        end
-end
-* Las llamadas anteriores siguen funcionando, ya que convierte el bloque en un proc al llamar a iterate! ahora.
-* Pero nos vamos a crear un proc que contiene el bloque de antes.
-
-cuadrado = Proc.new do |n|
-    n**2
-end
-
-array1.iterate! (cuadrado)
-array2.iterate! (cuadrado)
-
-array1.iterate! (lambda{|n| n**2}) # Tembien se puede con Lambda
-array1.iterate! (->(n) {n**2})     # Lambda sintaxis 2
-
-[**] EXPLICACION de la PRÁCTICA ...
-
-
-=end
